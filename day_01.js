3edb4dbe64ba50c85c8c751d402b3d8b95b8b686
Javeria <meetjaveriajavaid@gmail.com>
2025-07-31 23:29:36 +0500

    Variables
    Containers for Data - Variables are like labeled boxes where we store information in our program.

---
+++ day_01.js
@@ -0,0 +1,6 @@
+// Let's declare some variables
+let greeting = "Hello, future developer!";  // string
+const PI = 3.14;                           // constant number
+var score = 0;                             // old-school variable (we'll use 'let' instead)
+
+console.log(greeting);  // Prints to your browser console

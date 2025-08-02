97a9c6fbc76ad5cae100cee0b29579cd810a0663
Javeria <meetjaveriajavaid@gmail.com>
2025-08-03 01:23:08 +0500

    day_03 "Operators in js"

---
+++ day_03.js
@@ -0,0 +1,53 @@
+//JavaScript has 8 Datatypes
+//String
+//Number
+//Bigint
+//Boolean
+//Undefined
+//Null
+//Symbol
+//Object
+//Operators
+// 1. Arithmetic Operators (+, -, *, /)
+let a = 10;
+let b = 3;
+console.log(a + b); // ?
+console.log(a - b); // ?
+console.log(a * b); // ?
+console.log(a / b); // ?
+console.log(a % b); // ? (Modulus - remainder)
+
+let x = 5;
+let y = 2;
+let result = x + y * 2; // ?
+
+let z = (10 + 5) / 3 * 2; // ?
+
+// 2. Comparison Operators
+console.log(5 == "5");   // ?
+console.log(5 === "5");  // ?
+console.log(10 != "10"); // ?
+console.log(10 !== "10");// ?
+console.log(7 > 5);      // ?
+console.log(7 < 5);      // ?
+
+let age = 18;
+console.log(age >= 18); // ?
+console.log(age <= 17); // ?
+
+let num1 = 10;
+let num2 = "10";
+let isEqual = num1 === num2; // ?
+
+// 3. Logical Operators
+console.log(true && false);  // ?
+console.log(true || false);  // ?
+console.log(!true);          // ?
+
+let age = 20;
+let hasID = true;
+let canVote = age >= 18 && hasID; // ?
+
+let isAdmin = false;
+let isLoggedIn = true;
+let accessGranted = isAdmin || isLoggedIn; // ?
